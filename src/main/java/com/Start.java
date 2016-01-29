f5d701f66dc1e3c51c8f55ff0ef122f4f487113e
朱明明 <2428922347@qq.com>
2016-01-29 18:36:07 +0800

    Initial commit

---
+++ src/main/java/com/Start.java
@@ -0,0 +1,38 @@
+package com;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Module Desc:clover
+ * User: z.mm | 2428922347@qq.com
+ * Date: 2016/1/29
+ * Time: 15:14
+ * state:
+ */
+public class Start {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/*.xml");
+        context.start();
+        UserService userService = context.getBean(UserService.class);
+        System.out.println(userService.getUser(1L));
+    }
+}
